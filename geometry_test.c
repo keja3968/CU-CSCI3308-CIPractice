dd6a12f8a102759124f5d61e244c992f079bc517
user <keja3968@colorado.edu>
2018-11-30 13:20:41 -0700

    added test

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,58 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+/* coord_2d_triangle test*/
+START_TEST(test_2d_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    coord_2d_t exp;
+    coord_2d_t mid;
+    a.x = b.x = c.y = 0;
+    a.y = b.y = c.y = 0;	
+    mid = coord_2d_area_triangle(&a, &b, &c);
+    ck_assert(coord_2d_eq(&mid, &exp));
+
+    a.x = 0;
+    a.y = 0;
+    b.x = 3;
+    b.y = 0;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 1.5;
+    exp.y = 0;
+    ck_assert(coord_2d_eq(&mid, &exp));
+
+    a.x = 0;
+    a.y = 0;
+    b.x = 0;
+    b.y = 3;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 0;
+    exp.y = 1.5;
+    ck_assert(coord_2d_eq(&mid, &exp));
+
+    a.x = 0;
+    a.y = 0;
+    b.x = 3;
+    b.y = 3;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 1.5;
+    exp.y = 1.5;
+    ck_assert(coord_2d_eq(&mid, &exp));
+
+    a.x = 1;
+    a.y = 2;
+    b.x = 3;
+    b.y = 4;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 2;
+    exp.y = 3;
+    ck_assert(coord_2d_eq(&mid, &exp));
+
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -161,11 +213,14 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
-
+ 
+    TCase* tc_2d_triangle = tcase_create("coord_2d_triangle");
+    tcase_add_test(tc_2d_triangle, test_2d_triangle);
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_triangle);
 
     /* Return Suite */
     return s;
